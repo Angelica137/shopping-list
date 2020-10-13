a56d2efcec52e6a7fa2896e57f192e06b3fbef4d
Angelica <Angelica137@users.noreply.github.com>
2020-10-13 23:22:23 +0100

    import Lists into App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Lists from "./Lists";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Lists />
     </div>
   );
 }
