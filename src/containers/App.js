2dfcabdea5074a7dac8b5c62bc0fb612de3c8305
Angelica <Angelica137@users.noreply.github.com>
2020-10-14 21:03:24 +0100

    fix typo

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,13 +6,15 @@ import Lists from "./Lists";
 import List from "./List";
 
 const GlobalStyle = createGlobalStyle`
-body {
-	margin: 0;
-	padding: 0;
-	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 `;
 
 const AppWrapper = styled.div`
@@ -26,7 +28,7 @@ const App = () => (
       <Header />
       <Switch>
         <Route exact path="/" component={Lists} />
-        <Route path="list/:id" component={List} />
+        <Route path="/list/:id" component={List} />
       </Switch>
     </AppWrapper>
   </>
