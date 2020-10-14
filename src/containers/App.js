791d370243ecc3a768fba4a3402b3e2ed69b4ff2
Angelica <Angelica137@users.noreply.github.com>
2020-10-14 19:41:46 +0100

    import header into app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../components/Header/Header";
 import Lists from "./Lists";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Lists />
     </div>
   );
