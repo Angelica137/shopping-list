9ab75d1f8bd79760682fb46f9a3f1a9769457246
Angelica <Angelica137@users.noreply.github.com>
2020-10-14 20:15:14 +0100

    add router to app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,33 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { Route, Switch } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Lists from "./Lists";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Lists />
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+body {
+	margin: 0;
+	padding: 0;
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
 }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <AppWrapper>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Lists} />
+      </Switch>
+    </AppWrapper>
+  </>
+);
 
 export default App;
