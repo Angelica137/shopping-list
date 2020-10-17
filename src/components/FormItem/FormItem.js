8a0833416c74f760d53eb746fa8f2553800cb682
Angelica <Angelica137@users.noreply.github.com>
2020-10-18 00:42:53 +0100

    amend onChange in form to include =>

--- src/components/FormItem/FormItem.js
+++ src/components/FormItem/FormItem.js
@@ -39,7 +39,7 @@ const FormItem = ({
       id={id}
       placeholder={placeholder}
       value={value}
-      onChange={(e = handleOnChange(e.target.value))}
+      onChange={(e) => handleOnChange(e.target.value)}
     />
   </FormItemWrapper>
 );
